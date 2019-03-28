e41ec817cfbe76b59cdc066e9d88d4ed25652ad3
Jacob <jacobm3@gmail.com>
2019-03-28 15:20:34 -0500

    Changing zone to us-central1-b

--- infrastructure-as-code/k8s-cluster-gke/variables.tf
+++ infrastructure-as-code/k8s-cluster-gke/variables.tf
@@ -1,11 +1,11 @@
 variable "gcp_region" {
   description = "GCP region, e.g. us-east1"
-  default = "us-east1"
+  default = "us-central1"
 }
 
 variable "gcp_zone" {
   description = "GCP zone, e.g. us-east1-b (which must be in gcp_region)"
-  default = "us-east1-b"
+  default = "us-central1-b"
 }
 
 variable "gcp_project" {
